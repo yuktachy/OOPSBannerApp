5cfaba66fd0d20993da502f2b2c90253d161fa66
yuktachy <yuktachoudhary22@gmail.com>
2026-03-03 10:32:32 +0530

    coded UC2 to print large OOPS to console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,13 @@
 public class OOPSBannerApp {
     public static void main (String[] args) {
-        System.out.println("OOPS");
+       
+
+        System.out.println(" *****     *****     ******     ***** ");
+        System.out.println("*     *   *     *    *     *   *     *");
+        System.out.println("*     *   *     *    *     *   *      ");
+        System.out.println("*     *   *     *    ******     ***** ");
+        System.out.println("*     *   *     *    *               *");
+        System.out.println("*     *   *     *    *         *     *");
+        System.out.println(" *****     *****     *          ***** ");
     }
 }
